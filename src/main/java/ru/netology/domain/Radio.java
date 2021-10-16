127248497048be8f751c13c2b8a35e5b95a59844
Natalya Pletneva <nntalita@gmail.com>
2021-10-16 12:12:51 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,93 @@
+package ru.netology.domain;
+
+public class Radio {
+
+
+    private int currentVolume;
+    private int currentRadioStation;
+
+    //    количество доступных станций
+    public int getMaxStation() {
+
+        return 9;
+    }
+
+    //   предел громкости
+    public int getMaxVolume() {
+
+        return 10;
+    }
+// НАСТРОЙКА
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+
+    // установка текущей станции через прямое указание её номера
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > getMaxStation()) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+
+    }
+
+    //        переключение радиостанций при нажатии кнопки next
+    public void nextStationButton() {
+
+        if (currentRadioStation < getMaxStation()) {
+            this.currentRadioStation = currentRadioStation + 1;
+            return;
+        }
+        this.currentRadioStation = 0;
+
+    }
+
+    //        переключение радиостанций  при нажатии кнопки prev
+    public void prevStationButton() {
+
+        if (currentRadioStation > 0) {
+            this.currentRadioStation = currentRadioStation - 1;
+            return;
+        }
+        this.currentRadioStation = getMaxStation();
+
+    }
+
+    //    ГРОМКОСТЬ
+    public int getCurrentVolume() {
+
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+
+    }
+
+    //увеличение громкости
+
+    public void increaseVolume() {
+
+        if (currentVolume < getMaxVolume()) {
+            this.currentVolume = currentVolume + 1;
+            return;
+        }
+        this.currentVolume = getMaxVolume();
+    }
+
+
+
+//    уменьшение громкости
+
+    public void decreaseVolume() {
+
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+            return;
+        }
+        currentVolume = 0;
+    }
+}
+
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,84 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+        Radio radio = new Radio();
+//тестирую метод прямой установки номера станции
+//    @Test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataDirectInstallation.csv")
+
+    public void shouldCurrentRadioStationTest(int station, int expected) {
+//        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(station);
+        int actual = radio.getCurrentRadioStation();
+//        int expected = 0;
+        assertEquals(expected, actual);
+        System.out.println("прямая установка  " + "кнопка " + station);
+        System.out.print("ожидаемый результат " + expected);
+        System.out.println(" фактический результат " + actual);
+    }
+
+    //      тестирую метод переключения станции кнопкой next
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataNext.csv")
+    public void shouldNextStationButtonTest(int station, int expected) {
+//        Radio radio = new Radio();
+        radio.setCurrentRadioStation(station);
+        radio.nextStationButton();
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+        System.out.println("установленная станция "  + station + " кнопка +");
+        System.out.print("ожидаемый результат " + expected);
+        System.out.println(" фактический результат " + actual);
+    }
+
+    //      тестирую метод переключения станции кнопкой prev
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataPrev.csv")
+    public void prevStationButtonTest(int station, int expected) {
+//        Radio radio = new Radio();
+        radio.setCurrentRadioStation(station);
+        radio.prevStationButton();
+        int actual = radio.getCurrentRadioStation();
+        assertEquals(expected, actual);
+        System.out.println("установленная станция "  + station + " кнопка -");
+        System.out.print("ожидаемый результат " + expected);
+        System.out.println(" фактический результат " + actual);
+    }
+
+    //      тестирую метод переключения громкости +
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataVolume+.csv")
+    public void currentVolumeIncreaseTest(int volume, int expected) {
+//        Radio radio = new Radio();
+        radio.setCurrentVolume(volume);
+        radio.increaseVolume();
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+        System.out.println("установленная громкость "  + volume + " кнопка +");
+        System.out.print("ожидаемый результат " + expected);
+        System.out.println(" фактический результат " + actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataVolume-.csv")
+    public void currentVolumeDecreaseTest(int volume, int expected) {
+//        Radio radio = new Radio();
+        radio.setCurrentVolume(volume);
+        radio.decreaseVolume();
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+        System.out.println("установленная громкость "  + volume + " кнопка -");
+        System.out.print("ожидаемый результат " + expected);
+        System.out.println(" фактический результат " + actual);
+
+    }
+}
+
+
